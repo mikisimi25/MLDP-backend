190d18ebde24eadf97f2f03c9a23ac03a9b707e9
most274 <mikisimi25.dev@gmail.com>
2022-06-18 21:08:25 +0200

    v0.1.4 - auth

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -46,5 +46,13 @@ class UserSeeder extends Seeder
         $token = JWTAuth::fromUser($user);
         AppHelper::instance()->generateStarterPackLists( $user );
 
+        //Admin
+        $user = User::create([
+            'username' => 'root',
+            'email' => 'root@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('admin')->group(function () {
+    Auth::routes();
+    Route::get('home', [HomeController::class,'index']);
+});
+
+Route::get('', function () {
+    return redirect('/admin/login');
 });
